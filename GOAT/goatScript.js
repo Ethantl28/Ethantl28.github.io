c4cb299cf5cac8df417f4892ed315871f2a5d60b
Ethantl28 <ethantl28@outlook.com>
2026-06-13 00:24:23 +0100

    Added passwords, screens, custom file viewer, and more

--- GOAT/goatScript.js
+++ GOAT/goatScript.js
@@ -2,7 +2,7 @@ const bootLines =
 [
     "INITIALISING G.O.A.T...",
     "",
-    "LOADING ACRCHIVE INDEX...",
+    "LOADING ARCHIVE INDEX...",
     "",
     "ESTABLISHING SECURE CONNECTION...",
     "",
@@ -27,7 +27,7 @@ const screens =
 
     FILES:
     [
-        "AUDIO",
+        "AUDIO LOGS",
         "MUSIC",
         "DOCUMENTS",
         "BACK"
@@ -35,19 +35,26 @@ const screens =
 
     AUDIO:
     [
-        "AUDIO SCREEN",
+        "IT_BEGINS.MP3",
+        "DETECTIVE_CAMPBELL.MP3",
+        "GROUP_MEETING.MP3",
         "BACK"
     ],
 
     MUSIC:
     [
-        "MUSIC SCREEN",
+        "RECOVERED_01.MP3",
+        "RECOVERED_02.MP3",
+        "RECOVERED_03.MP3",
+        "RECOVERED_04.MP3",
         "BACK"
     ],
 
     DOCUMENTS:
     [
-        "DOCUMENTS SCREEN",
+        "EXPRESS_AND_STAR.PDF",
+        "MAP_SKETCH.JPG",
+        "EVIDENCE_BOARD_04.PDF",
         "BACK"
     ],
 
@@ -55,10 +62,83 @@ const screens =
     [
         "MAP SCREEN",
         "BACK"
+    ],
+
+    PASSWORD:
+    [
+        "BACK",
+        "PLEASE ENTER THE PASSWORD FOR THIS FILE"
     ]
 };
 
+const commands = 
+{
+    MAIN:
+    {
+        HELP: "HELP"
+    },
+
+    HELP:
+    {
+        FILES: "FILES",
+    },
+
+    FILES:
+    {
+        AUDIO: "AUDIO",
+        MUSIC: "MUSIC",
+        DOCUMENTS: "DOCUMENTS"
+    }
+}
+
+const unprotectedFiles = 
+{
+    AUDIO:
+    [
+        "IT_BEGINS.MP3"
+    ],
+
+    MUSIC:
+    [
+        "RECOVERED_01.MP3",
+        "RECOVERED_02.MP3",
+        "RECOVERED_03.MP3",
+        "RECOVERED_04.MP3"
+    ],
+}
+
+const protectedFiles = 
+{
+    AUDIO:
+    [
+        "DETECTIVE_CAMPBELL.MP3",
+        "GROUP_MEETING.MP3"
+    ],
+
+    DOCUMENTS:
+    [
+        "EXPRESS_AND_STAR.PDF",
+        "MAP_SKETCH.JPG",
+        "EVIDENCE_BOARD_04.PDF"
+    ]
+}
+
+const passwords = 
+{
+    "EXPRESS_AND_STAR.PDF": "FARCRY5",
+    "MAP_SKETCH.JPG": "THELOST",
+    "EVIDENCE_BOARD_04.PDF": "TOTTENHAM",
+    
+    "DETECTIVE_CAMPBELL.MP3": "PASSWORD",
+    "GROUP_MEETING.MP3": "LIVERPOOL",
+
+    "MAP": "1959"
+}
+
 let currentScreen = "MAIN";
+let previousScreen = "";
+let selectedFile = "";
+let ignoreInput = false;
 
 const keySound = new Audio ("Sounds/keyboard_click.mp3");
 const startupSound = new Audio ("Sounds/computer_startup.wav");
@@ -76,16 +156,12 @@ async function bootSequence()
 
     for (const line of bootLines)
     {
-        await typeLine(line);
+        await typeLine(line, "boot-output");
 
-        await new Promise(resolve =>
-            setTimeout(resolve, 700) // 700
-        );
+        await sleep(700);
     }
 
-    await new Promise(resolve =>
-        setTimeout(resolve, 1000) // 1000
-    );
+    await sleep(1000);
 
     
     document.getElementById("boot-screen").style.display = "none";
@@ -97,9 +173,9 @@ async function bootSequence()
     startupSound.play();
 }
 
-async function typeLine(text)
+async function typeLine(text, elementId)
 {
-    const output = document.getElementById("boot-output");
+    const output = document.getElementById(elementId);
 
     const p = document.createElement("p");
     output.appendChild(p);
@@ -111,15 +187,11 @@ async function typeLine(text)
 
         if (text[i] != ".")
         {
-            await new Promise(resolve =>
-                setTimeout(resolve, 50) // 50
-            );
+            await sleep(50);
         }
         else
         {
-            await new Promise(resolve =>
-                setTimeout(resolve, 500) // 500
-            );
+            await sleep(500);
         }
     }
 }
@@ -136,7 +208,10 @@ terminalInput.addEventListener("keydown", function(event)
 {
     if (event.key === "Enter")
     {
-        processCommand(terminalInput.value);
+        if (!ignoreInput)
+        {
+            processCommand(terminalInput.value);
+        }
 
         terminalInput.value = "";
     }
@@ -146,9 +221,8 @@ function processCommand(command)
 {
     command = command.trim().toUpperCase();
 
-    if (command === "HELP")
+    if (command === "")
     {
-        showScreen("HELP");
         return;
     }
 
@@ -158,54 +232,64 @@ function processCommand(command)
         return;
     }
 
-    if (currentScreen === "HELP")
-    {
-        switch (command)
-        {
-            case "FILES":
-                showScreen("FILES");
-                return;
-            
-            case "MAP":
-                showScreen("MAP");
-                return;
+    const screenCommands = commands[currentScreen];
 
-            default:
-                showUnknownCommand();
-                return;
-        }
+    if (screenCommands && screenCommands[command])
+    {
+        showScreen(screenCommands[command]);
+        return;
     }
 
-    if (currentScreen === "FILES")
+    if (protectedFiles[currentScreen]?.includes(command))
     {
-        switch (command)
-        {
-            case "AUDIO":
-                showScreen("AUDIO");
-                return;
+        previousScreen = currentScreen;
 
-            case "MUSIC":
-                showScreen("MUSIC");
-                return;
+        selectedFile = command;
 
-            case "DOCUMENTS":
-                showScreen("DOCUMENTS");
-                return;
+        showScreen("PASSWORD");
 
-            default:
-                showUnknownCommand();
-                return;
-        }
+        return;
     }
 
-    if (command != "")
+    if (unprotectedFiles[currentScreen]?.includes(command))
     {
-        showUnknownCommand();
+        previousScreen = currentScreen;
+
+        selectedFile = command;
+
+        openFile(selectedFile, true);
+
+        return;
     }
+
+    if (currentScreen === "PASSWORD")
+    {
+        testPassword(command);
+        return;
+    }
+
+    if (currentScreen === "HELP" && command === "MAP")
+    {
+        selectedFile = "MAP";
+        previousScreen = "HELP";
+
+        showScreen("PASSWORD");
+
+        return;
+    }
+
+    showUnknownCommand();
 }
 
 function back()
 {
+    if (previousScreen != "")
+    {
+        showScreen(previousScreen);
+        previousScreen = "";
+        return;
+    }
+
     switch (currentScreen)
     {
         case "AUDIO":
@@ -228,6 +312,97 @@ function back()
 function showUnknownCommand()
 {
     printWithTimeout("ERROR: UNKNOWN COMMAND", 2500);
+}
+
+async function testPassword(attemptedPassword)
+{
+    if (attemptedPassword === passwords[selectedFile])
+    {
+        await openFile(selectedFile, false);
+    }
+    else
+    {
+        printWithTimeout("INCORRECT PASSWORD", 2500);
+    }
+}
+
+async function openFile(file, unprotectedFile)
+{
+    clearTerminal();
+
+    hideTerminalInput(true);
+
+    if (unprotectedFile)
+    {
+        await typeLine("ACCESS GRANTED TO BIRTHDAY_BOY...", "terminal-output")
+        await sleep(700);
+        await typeLine("OPENING FILE...", "terminal-output");
+        await sleep(500);
+    }
+    else
+    {
+        await typeLine("PASSWORD ACCEPTED...", "terminal-output");
+        await sleep(700);
+        await typeLine("ACCESS GRANTED TO BIRTHDAY_BOY...", "terminal-output")
+        await sleep(700);
+        await typeLine("OPENING FILE...", "terminal-output");
+        await sleep(500);
+    }
+
+    openViewer(`${previousScreen}/${file}`, file);
+
+    showScreen(previousScreen);
+
+    previousScreen = "";
+}
+
+function openViewer(filePath, fileName)
+{
+    const viewer = document.getElementById("file-viewer")
+    
+    viewer.style.display = "flex";
+    viewer.classList.add("active");
+
+    document.getElementById("file-name").textContent = fileName;
+
+    document.getElementById("file-frame").src = filePath;
+}
+
+function closeViewer()
+{
+    const viewer = document.getElementById("file-viewer")
+    
+    viewer.style.display = "none";
+    viewer.classList.remove("active");
+
+    document.getElementById("file-frame").src = "";
+
+    hideTerminalInput(false);
+}
+
+function hideTerminalInput(hide)
+{
+    const terminalInput = document.querySelector(".terminal-input-row");
+    if (hide)
+    {
+        terminalInput.style.display = "none";
+    }
+    else
+    {
+        terminalInput.style.display = "flex";
+    }
+}
+
+document    
+    .getElementById("close-file")
+    .addEventListener("click", () =>
+    {
+        closeViewer();
+    });
+
+function sleep(ms)
+{
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 function showScreen(screen)
@@ -263,9 +438,7 @@ async function printWithTimeout(text, time)
 
     output.appendChild(line);
 
-    await new Promise(resolve =>
-                setTimeout(resolve, time)
-    );
+    await sleep(time);
 
     line.textContent = "";
 }
@@ -288,12 +461,18 @@ async function skipBoot()
 
 document.addEventListener("DOMContentLoaded", () =>
 {
-    // skipBoot();
-    document
-        .getElementById("launch-screen")
-        .addEventListener("click", startSystem, {once: true});
-
-    document
-        .getElementById("launch-screen")
-        .addEventListener("touchStart", startSystem, {once: true});
+    if (true)
+    {
+        skipBoot();
+    }
+    else
+    {
+        document
+            .getElementById("launch-screen")
+            .addEventListener("click", startSystem, {once: true});
+    
+        document
+            .getElementById("launch-screen")
+            .addEventListener("touchstart", startSystem, {once: true});
+    }
 })
